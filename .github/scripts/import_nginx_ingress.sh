a060bfb7f7d783bd0e6651a354b06c1d446c6214
Asger Thyregod <asger.thyregod@gmail.com>
2025-07-30 16:26:03 +0200

    Trying to import nginx into own ACR

---
+++ .github/scripts/import_nginx_ingress.sh
@@ -0,0 +1,22 @@
+REGISTRY_NAME=${{ env.acr_registry }}
+SOURCE_REGISTRY=registry.k8s.io
+CONTROLLER_IMAGE=ingress-nginx/controller
+CONTROLLER_TAG=v1.8.1
+PATCH_IMAGE=ingress-nginx/kube-webhook-certgen
+PATCH_TAG=v20230407
+DEFAULTBACKEND_IMAGE=defaultbackend-amd64
+DEFAULTBACKEND_TAG=1.5
+
+if ! az acr check-name -n $CONTROLLER_IMAGE:$CONTROLLER_TAG; then
+    echo "importing $CONTROLLER_IMAGE:$CONTROLLER_TAG ..."
+    az acr import --name $REGISTRY_NAME --source $SOURCE_REGISTRY/$CONTROLLER_IMAGE:$CONTROLLER_TAG --image $CONTROLLER_IMAGE:$CONTROLLER_TAG
+    echo "imported $CONTROLLER_IMAGE:$CONTROLLER_TAG !"
+else
+    echo "$CONTROLLER_IMAGE:$CONTROLLER_TAG already imported. Skipping apply."
+fi
+
+#az acr check-name -n $PATCH_IMAGE:$PATCH_TAG
+#az acr check-name -n $PATCH_IMAGE:$PATCH_TAG
+
+#az acr import --name $REGISTRY_NAME --source $SOURCE_REGISTRY/$PATCH_IMAGE:$PATCH_TAG --image $PATCH_IMAGE:$PATCH_TAG
+#az acr import --name $REGISTRY_NAME --source $SOURCE_REGISTRY/$DEFAULTBACKEND_IMAGE:$DEFAULTBACKEND_TAG --image $DEFAULTBACKEND_IMAGE:$DEFAULTBACKEND_TAG
